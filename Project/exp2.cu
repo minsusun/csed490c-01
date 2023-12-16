b94c4a06fda978e19c0ea9f4b824124b01cc6e14
minsusun <poodding397@gmail.com>
2023-12-17 03:17:22 +0900

    update

--- Project/exp2.cu
+++ Project/exp2.cu
@@ -5,10 +5,8 @@
 #include <algorithm>
 #include <ctime>
 
-#include <thrust/sort.h>
-#include <thrust/execution_policy.h>
-
 #define TILE_WIDTH 32
+#define THREADS_PER_BLOCK 512
 using namespace std;
 
 #define CUDACheck(stmt)                                                     \
@@ -21,13 +19,13 @@ using namespace std;
     }                                                                       \
   } while (0)
 
-string name = "CUDA(pinned memory/matrix distance calculation/thrust sort)";
+string name = "CUDA(pinned memory/matrix distance calculation/odd-even sort)";
 
 double interval(clock_t *p) {
-    clock_t t = clock();
-    double result = double(t - *p) / CLOCKS_PER_SEC * 1000;
-    *p = t;
-    return result;
+  clock_t t = clock();
+  double result = double(t - *p) / CLOCKS_PER_SEC * 1000;
+  *p = t;
+  return result;
 }
 
 __global__ void matrixDistance(double *X, double *X_t, double *D, int *idxMat, int N, int dim) {
@@ -66,98 +64,128 @@ __global__ void matrixDistance(double *X, double *X_t, double *D, int *idxMat, i
   if (Row < N && Col < N) D[Row * N + Col] = sqrt(Cvalue), idxMat[Row * N + Col] = Col;
 }
 
+__global__ void oddEvenSort(double *values, int *keys, int t, int size) {
+  int idx = blockDim.x * blockIdx.x + threadIdx.x;
+  if(!t && idx * 2 + 1 < size) {
+    if(values[idx * 2] > values[idx * 2 + 1]) {
+      double tmp = values[idx * 2];
+      values[idx * 2] = values[idx * 2 + 1];
+      values[idx * 2 + 1] = tmp;
+      int i = keys[idx * 2];
+      keys[idx * 2] = keys[idx * 2 + 1];
+      keys[idx * 2 + 1] = i;
+    }
+  }
+  else if(idx * 2 + 2 < size){
+    if(values[idx * 2 + 1] > values[idx * 2 + 2]) {
+      double tmp = values[idx * 2 + 1];
+      values[idx * 2 + 1] = values[idx * 2 + 2];
+      values[idx * 2 + 2] = tmp;
+      int i = keys[idx * 2 + 1];
+      keys[idx * 2 + 1] = keys[idx * 2 + 2];
+      keys[idx * 2 + 2] = i;
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
-    assert(argc == 6);
+  assert(argc == 6);
 
-    int N = atoi(argv[1]);
-    int dim = atoi(argv[2]);
-    int K = atoi(argv[3]);
-    int *IdxMat;
-    int *dIdxMat;
-    double *X;
-    double *X_t;
-    double *dX;
-    double *dX_t;
-    double *D;
-    double *dD;
-    string title;
-    clock_t p;
+  int N = atoi(argv[1]);
+  int dim = atoi(argv[2]);
+  int K = atoi(argv[3]);
+  int *IdxMat;
+  int *dIdxMat;
+  double *X;
+  double *X_t;
+  double *dX;
+  double *dX_t;
+  double *D;
+  double *dD;
+  string title;
+  clock_t p;
 
-    CUDACheck(cudaMallocHost((void **)&X, N * dim * sizeof(double)));
-    CUDACheck(cudaMallocHost((void **)&X_t, dim * N * sizeof(double)));
-    CUDACheck(cudaMallocHost((void **)&D, N * N * sizeof(double)));
-    CUDACheck(cudaMallocHost((void **)&IdxMat, N * N * sizeof(int)));
+  CUDACheck(cudaMallocHost((void **)&X, N * dim * sizeof(double)));
+  CUDACheck(cudaMallocHost((void **)&X_t, dim * N * sizeof(double)));
+  CUDACheck(cudaMallocHost((void **)&D, N * N * sizeof(double)));
+  CUDACheck(cudaMallocHost((void **)&IdxMat, N * N * sizeof(int)));
 
-    CUDACheck(cudaMalloc((void **)&dX, N * dim * sizeof(double)));
-    CUDACheck(cudaMalloc((void **)&dX_t, dim * N *sizeof(double)));
-    CUDACheck(cudaMalloc((void **)&dD, N * N * sizeof(double)));
-    CUDACheck(cudaMalloc((void **)&dIdxMat, N * N * sizeof(int)));
-    
-    cout << name << endl;
-    cout << "N=" << N << " dim=" << dim << " K=" << K << " " << argv[4] << endl;
+  CUDACheck(cudaMalloc((void **)&dX, N * dim * sizeof(double)));
+  CUDACheck(cudaMalloc((void **)&dX_t, dim * N *sizeof(double)));
+  CUDACheck(cudaMalloc((void **)&dD, N * N * sizeof(double)));
+  CUDACheck(cudaMalloc((void **)&dIdxMat, N * N * sizeof(int)));
+  
+  cout << name << endl;
+  cout << "N=" << N << " dim=" << dim << " K=" << K << " " << argv[4] << endl;
 
-    p = clock();
+  p = clock();
 
-    ifstream fin;
-    fin.open(argv[4]);
+  ifstream fin;
+  fin.open(argv[4]);
 
-    fin >> title;
+  fin >> title;
 
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < dim; j++) {
-            fin >> X[i * dim + j];
-            X_t[j * N + i] = X[i * dim + j];
-        }
+  for(int i = 0; i < N; i++) {
+    for(int j = 0; j < dim; j++) {
+      fin >> X[i * dim + j];
+      X_t[j * N + i] = X[i * dim + j];
     }
+  }
 
-    fin.close();
-    
-    cout << "step 0:Data Import::" << interval(&p) << "ms" << endl;
+  fin.close();
+  
+  cout << "step 0:Data Import::" << interval(&p) << "ms" << endl;
 
-    CUDACheck(cudaMemcpy(dX, X, N * dim * sizeof(double), cudaMemcpyHostToDevice));
-    CUDACheck(cudaMemcpy(dX_t, X_t, dim * N * sizeof(double), cudaMemcpyHostToDevice));
+  CUDACheck(cudaMemcpy(dX, X, N * dim * sizeof(double), cudaMemcpyHostToDevice));
+  CUDACheck(cudaMemcpy(dX_t, X_t, dim * N * sizeof(double), cudaMemcpyHostToDevice));
 
-    cout << "step 1:Memcpy H2D::" << interval(&p) << "ms" << endl;
+  cout << "step 1:Memcpy H2D::" << interval(&p) << "ms" << endl;
 
-    dim3 grid_1((N - 1) / TILE_WIDTH + 1, (N - 1) / TILE_WIDTH + 1, 1);
-    dim3 block_1(TILE_WIDTH, TILE_WIDTH, 1);
-    size_t shm_size = 2 * TILE_WIDTH * TILE_WIDTH * sizeof(double);
-    matrixDistance<<<grid_1, block_1, shm_size>>>(dX, dX_t, dD, dIdxMat, N, dim);
+  dim3 grid_1((N - 1) / TILE_WIDTH + 1, (N - 1) / TILE_WIDTH + 1, 1);
+  dim3 block_1(TILE_WIDTH, TILE_WIDTH, 1);
+  size_t shm_size = 2 * TILE_WIDTH * TILE_WIDTH * sizeof(double);
+  matrixDistance<<<grid_1, block_1, shm_size>>>(dX, dX_t, dD, dIdxMat, N, dim);
 
-    CUDACheck(cudaDeviceSynchronize());
+  CUDACheck(cudaDeviceSynchronize());
 
-    cout << "step 2:Distance::" << interval(&p) << "ms" << endl;
+  cout << "step 2:Distance::" << interval(&p) << "ms" << endl;
 
-    for(int i = 0; i < N; i++) thrust::sort_by_key(thrust::device, dD + i * N, dD + (i + 1) * N, dIdxMat + i * N);
+  dim3 grid_2((N / 2 - 1) / THREADS_PER_BLOCK + 1, 1, 1);
+  dim3 block_2(THREADS_PER_BLOCK, 1, 1);
 
-    cout << "step 3:Sort::" << interval(&p) << "ms" << endl;
+  for(int i = 0; i < N; i++) {
+    for(int j = 0; j < N; j++) oddEvenSort<<<grid_2, block_2>>>(dD + i * N, dIdxMat + i * N, j % 2, N);
+  }
 
-    CUDACheck(cudaMemcpy(IdxMat, dIdxMat, N * N * sizeof(int), cudaMemcpyDeviceToHost));
+  CUDACheck(cudaDeviceSynchronize());
 
-    cout << "step 4:Memcpy D2H::" << interval(&p) << "ms" << endl;
+  cout << "step 3:Sort::" << interval(&p) << "ms" << endl;
 
-    ofstream fout;
-    fout.open(string(argv[5]));
+  CUDACheck(cudaMemcpy(IdxMat, dIdxMat, N * N * sizeof(int), cudaMemcpyDeviceToHost));
 
+  cout << "step 4:Memcpy D2H::" << interval(&p) << "ms" << endl;
 
-    for(int i = 0; i < N; i++) {
-        // omit first one -> i-i pair
-        for(int j = 1; j < K + 1; j++) fout << IdxMat[i * N + j] << " ";
+  ofstream fout;
+  fout.open(string(argv[5]));
 
-        fout << endl;
-    }
+  for(int i = 0; i < N; i++) {
+    // omit first one -> i-i pair
+    for(int j = 1; j < K + 1; j++) fout << IdxMat[i * N + j] << " ";
 
-    fout.close();
+    fout << endl;
+  }
 
-    cout << "step 5:Export Result::" << interval(&p) << "ms" << endl;
+  fout.close();
 
-    cudaFreeHost(X);
-    cudaFreeHost(X_t);
-    cudaFreeHost(D);
-    cudaFreeHost(IdxMat);
+  cout << "step 5:Export Result::" << interval(&p) << "ms" << endl;
 
-    cudaFree(dX);
-    cudaFree(dX_t);
-    cudaFree(dD);
-    cudaFree(dIdxMat);
+  cudaFreeHost(X);
+  cudaFreeHost(X_t);
+  cudaFreeHost(D);
+  cudaFreeHost(IdxMat);
+
+  cudaFree(dX);
+  cudaFree(dX_t);
+  cudaFree(dD);
+  cudaFree(dIdxMat);
 }
